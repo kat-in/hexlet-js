4d90a52e206f0d071b97501ce389fc348f9d5171
kat-in <newkatelife@gmail.com>
2025-01-24 21:12:08 +0300

    Использование Фикстур

--- hexlet-jest/__tests__/index.test.js
+++ hexlet-jest/__tests__/index.test.js
@@ -1,6 +1,23 @@
-import reverse from '../src/index.js';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+import fs from "fs";
+import reverse from "../src/index.js";
 
-test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), "utf-8");
+
+const text = readFile("text.html");
+const result = readFile("result.html");
+
+test("reverse", () => {
+  expect(reverse("hello")).toEqual("olleh");
+  expect(reverse("")).toEqual("");
+  expect(reverse(text.trim())).toEqual(result.trim());
 });
